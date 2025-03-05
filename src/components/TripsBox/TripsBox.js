ac4c4b2b90ee0bc634ac07e77faee223b3891e99
mahmoudnadim <mahmoudnadim51@gmail.com>
2025-03-05 04:08:36 +0300

    updates

--- src/components/TripsBox/TripsBox.js
+++ src/components/TripsBox/TripsBox.js
@@ -4,7 +4,7 @@ export default function TripsBox(props) {
   return (
     <div className="BSItems lg:!w-1/3 xl:!w-1/4 flex-none">
       <div className="BSI pr-6 max-sm:pr-0">
-        <a href={"/trip/" + props.id}>
+        <a href={"trip/" + props.id}>
           <img className="BSImg rounded-lg aspect-video object-cover" src={props.img} alt="" />
         </a>
         <div className="BSIContent flex justify-between items-center py-2">
